a0480398887cb983ca175d6cdd68cda9278da085
Wilian Ribeiro <wilian.ribeiro@itspoa.com>
2020-04-06 20:51:44 -0300

    ignore conexãpo

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,19 +1,7 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:bater123@localhost:5432/combateCovid'
+  connectionString: process.env.DATABASE_URL 
 });
-
-// Criar nova conexão
-// const connection = new Pool({
-//   user: 'postgres',         //Usuário do Banco
-//   host: 'localhost',        //host
-//   database: 'combateCovid', //Nome do Banco
-//   password: 'bater123',     // Senha do Banco
-//   port: 5432,               //Porta do Banco em Operação
-// })
-
-console.log(connection);
-
 
 module.exports = connection;
