5f6a1effe285003ded8f3ed1ef32d8169ee43162
Wilian Ribeiro <wilian.ribeiro@itspoa.com>
2020-04-06 20:22:03 -0300

    configurado connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:bater123@localhost:5432/combateCovid'
+  connectionString: process.env.DATABASE_URL
 });
 
 // Criar nova conexão
